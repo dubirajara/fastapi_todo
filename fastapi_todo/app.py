c51d53903e5f5e85408d3ff0a60b377239e9e5ef
Diego Ubirajara <diego.ubirajara@mail.com>
2024-06-19 16:09:46 +0200

    cambios aula 2

--- fastapi_todo/app.py
+++ fastapi_todo/app.py
@@ -1,8 +1,27 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fastapi_todo.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Hello World'}
+
+
+@app.get('/hello', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def hello():
+    return """
+    <html>
+        <head>
+            <title>Hello World</title>
+        </head>
+        <body>
+            <h1>Hello World</h1>
+        </body>
+    </html>
+    """
--- tests/test_app.py
+++ tests/test_app.py
@@ -11,3 +11,19 @@ def test_read_root_status_code_and_message_ok():
     response = client.get('/')
     assert response.json() == {'message': 'Hello World'}
     assert response.status_code == HTTPStatus.OK
+
+
+def test_read_hello_status_code_and_message_ok():
+    response = client.get('/hello')
+    assert response.text == """
+    <html>
+        <head>
+            <title>Hello World</title>
+        </head>
+        <body>
+            <h1>Hello World</h1>
+        </body>
+    </html>
+    """
+    assert response.status_code == HTTPStatus.OK
+    assert response.headers['content-type'] == 'text/html; charset=utf-8'
