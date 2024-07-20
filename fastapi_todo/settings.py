2a3add14e672afe109cbae365bc89bb22dd56043
Diego Ubirajara <diego.ubirajara@mail.com>
2024-07-20 15:54:14 +0200

    aula 10

--- fastapi_todo/settings.py
+++ fastapi_todo/settings.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file='.env',
         env_file_encoding='utf-8',
+        extra='ignore'
     )
     DATABASE_URL: str
     SECRET_KEY: str
